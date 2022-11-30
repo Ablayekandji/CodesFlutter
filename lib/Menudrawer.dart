cc452628960cb42b31ee61b722dfa95338faf594
Ablayekandji <kandji.djibrilabdoulaye@gmail.com>
2022-12-01 00:47:59 +0200

    menu au complet https://www.youtube.com/watch?v=FSlXondkZzc

--- lib/Menudrawer.dart
+++ lib/Menudrawer.dart
@@ -22,16 +22,67 @@ class _HomeMenudrawer extends State<Menudrawer> {
         ),
       ),
       drawer: Drawer(
-        child: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: [
-                MyHeaderDrawer(),
-                MyHeaderList(),
-              ],
+        child: ListView(
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.all(12),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(14)),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 10, top: 10),
+                      height: 70,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            fit: BoxFit.contain,
+                            image: AssetImage('assets/images/profile.jpg'),
+                          )),
+                    ),
+                    Text(
+                      "Niania nonio",
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ],
+                ),
+              ),
             ),
-          ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Accueil"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.person_outline),
+              title: Text("profile"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.sailing),
+              title: Text("Vente"),
+              onTap: () {},
+            ),
+            Padding(padding: EdgeInsets.all(56)),
+            ListTile(
+              leading: Icon(Icons.logout_rounded),
+              title: Text("Se deconnecter"),
+              onTap: () {},
+            ),
+          ],
         ),
+        // child: SingleChildScrollView(
+        //   child: Container(
+        //     child: Column(
+        //       children: [
+        //         MyHeaderDrawer(),
+        //         MyHeaderList(),
+        //       ],
+        //     ),
+        //   ),
+        // ),
       ),
     );
   }
